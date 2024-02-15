c294fa1194fbd53e62b26a5ddecb10b5af32f12f
HeTron <jcode2k@gmail.com>
2024-02-14 19:50:13 -0500

    changed path

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, render_template, jsonify
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
 # Load your trained model
-model_path = '../ML_Web_App_Flask/models/bitcoin_price_model.pkl'
+base_dir = os.path.abspath(os.path.dirname(__file__))
+model_path = os.path.join(base_dir, '../../ML_Web_App_Flask/models/bitcoin_price_model.pkl')
+
 with open(model_path, 'rb') as model_file:
     model = pickle.load(model_file)
 
